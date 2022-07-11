8a6772c4bc5bd72f5ee15f9043299d2091960ba4
fanyingze <761545737@qq.com>
2022-07-11 15:12:49 +0800

    fix: npm包整合发布

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
-import Button from 'components/Button.vue'
-import ButtonGroup from 'components/ButtonGroup.vue'
+import Button from 'components/Button/Button.vue'
+import ButtonGroup from 'components/Button/ButtonGroup.vue'
 import Icon from 'components/Icon.vue'
+import Col from 'components/Layout/Col.vue'
+import Row from 'components/Layout/Row.vue'
+
+import MessageBox from 'plugin/MessageBox/MessageBox.vue'
 
 const script = document.createElement("script")
 script.type = 'text/javascript'
 script.src = "http://at.alicdn.com/t/font_3455135_g7o0v0181z5.js"
 document.getElementsByTagName('body')[0].appendChild(script)
 
-export {Button,ButtonGroup,Icon}
-
+export {Button,ButtonGroup,Icon,Col,Row,MessageBox}
